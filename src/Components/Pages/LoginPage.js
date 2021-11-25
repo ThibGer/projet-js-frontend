73ddf25d3a58e36089961b5351afa8e8bc0fdcb7
Morgoht <amauryandrade1998@gmail.com>
2021-11-25 11:22:09 +0100

    login front

--- src/Components/Pages/LoginPage.js
+++ src/Components/Pages/LoginPage.js
@@ -1,1 +1,10 @@
 // import la navbar.
+// import les fonction utiles.
+
+const shesh = ´hello´;
+   
+function loginPage() {
+    const pageDiv = document.querySelector('#page');
+pageDiv.innerHTML = shesh;
+}
+
--- src/index.js
+++ src/index.js
@@ -2,3 +2,26 @@
 // sur le index.js, il faut import le CDN bootstrap
 // import tout les component "import HomePage from "lien direct""
 // footer, heade, lougout, Pages (loginPage, HomePage,RegisterPage,AddMap).
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./stylesheets/style.css";
+
+import HomePage from "./Components/Pages/HomePage";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import {Router} from "./Components/Router/Router";
+
+// Improve the header with dynamic HTML generation
+Header();
+
+/// Render the Navbar
+Navbar();
+
+// Render the HomePage
+// HomePage();
+
+// Improve the header with dynamic HTML generation
+Footer();
+
+// Call the router
+Router();
