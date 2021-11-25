8a608ffb21e2ccb24c4df29703756ee56e9f27f5
Morgoht <amauryandrade1998@gmail.com>
2021-11-25 08:59:52 +0100

    ajout de dossier

---
+++ src/index.js
@@ -0,0 +1,4 @@
+
+// sur le index.js, il faut import le CDN bootstrap
+// import tout les component "import HomePage from "lien direct""
+// footer, heade, lougout, Pages (loginPage, HomePage,RegisterPage,AddMap).
